3265e34ceb9e4167f29c9cf917742e2639531522
G9-code <76937464+G9-code@users.noreply.github.com>
2021-12-15 20:13:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(600, 400);
+    canvas.position(450, 235);
+}
+function draw() {
+
+}
+function take_snapshot() {
+    save("snapshot.png");
+}
